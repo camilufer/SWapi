459277ec8a8c88dbecb5367ad6ae54cb7f6a28a1
camilufer <camilu.fer21@gmail.com>
2018-02-19 00:56:25 -0300

    js editado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,49 @@
-//agregando evento al boton de buscar
-$('#button').click(function(){
-  const pokeName = $('#search-bar').val();
-  const pokeNormal = pokeName.toLowerCase();
-  $.ajax({
-    url: `https://swapi.co/api/people/`,
-    type: 'GET',
-    datatype: 'json',
-    success: function(results){
-      console.log(results);
-    }
-  })
-  .done(response)
-  .fail(error);
-  function response(data) {
-    $('#swinfo').empty();
-    const swPicture = ""
-    const swName = data.name;
-    const swHeight = data.height;
-    const swGender = data.gender;
-    const swmass = data.mass;
-    const swbirthYear = data.birth_year;
-    $('#modalswInfo').empty();
-    $('#swnfo').append(`<img class="poke-imagen poke-img-modal" src="${swPicture}" data-toggle="modal" data-target="#modalPokeInfo"><h4><strong>${data.name}</strong></h4>`);
-    $('#modalswInfo').append(`<div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h3 class="modal-title">Información </h3></div><div class="modal-body"><p>Número de Pokedex: ${swHeight}</p><p>Tipo: ${swGender}</p><p>Peso: ${swmass}</p><p>Altura: ${swPicture}</p><p>Experiencia base: ${swGender}</p><p>Habilidades: ${swName}</p></div></div></div>`)
-  }
+let button 		= document.querySelector('#button')
+let name 		= document.querySelector('#name')
+let height 		= document.querySelector('#height')
+let gender 		= document.querySelector('#gender')
+let mass 		= document.querySelector('#mass')
 
-  function error(){
-    alert('Ha ocurrido un error')
-  }
-})
+
+
+function getInfo() {
+  updateWithLoading()
+  let randomNumber = Math.floor((Math.random() * 88) + 1)
+  let apiUrl = 'https://swapi.co/api/people/' + randomNumber
+
+	axios.get(apiUrl).then(response => {
+    updateInfo(response.data)
+  }).catch(e => {
+    updateInfoError()
+  })
+}
+
+function updateInfo(data) {
+	name.innerText = data.name
+	height.innerText = `Height: ${data.height}`
+	gender.innerText = `Gender: ${data.gender}`
+	mass.innerText = `Mass: ${data.mass}`
+	
+
+}
+
+function updateInfoError() {
+	name.innerText = 'Oh no! That person isnt available.'
+	height.innerText = ''
+	gender.innerText = ''
+	mass.innerText = ''
+	
+	
+}
+
+function updateWithLoading() {
+	name.innerHTML = '<i class="fa fa-spinner fa-spin" style="font-size:24px"></i>'
+	height.innerText = ''
+	gender.innerText = ''
+	mass.innerText = ''
+
+	
+}
+
+button.addEventListener('click', getInfo)
+
